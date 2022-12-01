3053200992596d6e6f36d6026785929cef36d9b6
valeriaotr <valeryaotr@gmail.com>
2022-12-01 16:13:58 +0300

    fixing

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -41,17 +41,7 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    a = []
-    b = []
-    r = 0
-    for i in range(0, len(values)):
-        r += 1
-        b.append(values[i])
-        if r == n:
-            a.append(b)
-            b = []
-            r = 0
-    return a
+    pass
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
@@ -63,7 +53,7 @@ def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str
     >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
     ['.', '8', '9']
     """
-    return grid[pos[0]]
+    pass
 
 
 def get_col(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
@@ -75,12 +65,7 @@ def get_col(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str
     >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
     ['3', '6', '9']
     """
-    a = []
-    for i in grid:
-        for j in range(0, len(grid)):
-            if j == pos[1]:
-                a.append(i[j])
-    return a
+    pass
 
 
 def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
@@ -93,13 +78,7 @@ def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[s
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    a = int(pos[0]) // 3 * 3
-    b = int(pos[1]) // 3 * 3
-    c = []
-    for i in range(a, a + 3):
-        for j in range(b, b + 3):
-            c.append(grid[i][j])
-    return c
+    pass
 
 
 def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[int, int]]:
@@ -111,11 +90,7 @@ def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[in
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    for i in range(0, len(grid)):
-        for j in range(0, len(grid[0])):
-            if grid[i][j] == ".":
-                return (i, j)
-    return "no"
+    pass
 
 
 def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.Set[str]:
@@ -128,49 +103,7 @@ def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -
     >>> values == {'2', '5', '9'}
     True
     """
-    q = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-    block = get_block(grid, pos)
-    for i in range(0, len(block)):
-        for j in range(0, len(block[1])):
-            if block[i][j] in q:
-                q.remove(block[i][j])
-    row = get_row(grid, pos)
-    for i in range(0, len(row)):
-        if row[i] in q:
-            q.remove(row[i])
-    column = get_col(grid, pos)
-    for i in range(0, len(column)):
-        if column[i] in q:
-            q.remove(column[i])
-    return q
-
-
-def var(grid: tp.List[tp.List[str]]):
-    f.clear()
-    for i in range(0, len(grid)):
-        t = []
-        for j in range(0, len(grid[0])):
-            t.append(grid[i][j])
-        f.append(t)
-
-
-def solver(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]):
-    global f
-    position = find_empty_positions(grid)
-    if position == "no":
-        var(grid)
-        return
-    values = find_possible_values(grid, (int(position[0]), int(position[1])))
-    if find_empty_positions(grid) == "no":
-        return
-
-    if len(values) == 0:
-        grid[int(pos[0])][int(pos[1])] = "."
-        return
-    for value in values:
-        grid[int(position[0])][int(position[1])] = str(value)
-        solver(grid, (position[0], position[1]))
-        grid[int(position[0])][int(position[1])] = "."
+    pass
 
 
 def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
@@ -185,33 +118,13 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    solver(grid, (find_empty_positions(grid)[0], find_empty_positions(grid)[1]))
-    a = f
-    return a
+    pass
 
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     """ Если решение solution верно, то вернуть True, в противном случае False """
-    r: tp.List[str] = []
-    q = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    for i in range(0, len(solution)):
-        for j in range(0, len(q)):
-            if solution[i].count(q[j]) > 1:
-                return False
-    for i in range(0, len(solution)):
-        r.clear()
-        for j in range(0, len(q)):
-            r.append(solution[j][i])
-        for t in range(0, len(q)):
-            if r.count(q[i]) > 1:
-                return False
-    for i in range(0, len(solution)):
-        for j in range(0, len(solution[0])):
-            if solution[i][j] == ".":
-                return False
-    # block = get_block(grid,)
-
-    return True
+    # TODO: Add doctests with bad puzzles
+    pass
 
 
 def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
@@ -235,26 +148,7 @@ def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
     >>> check_solution(solution)
     True
     """
-    r: tp.List[str] = []
-    q = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    for i in range(0, len(solution)):
-        for j in range(0, len(q)):
-            if solution[i].count(q[j]) > 1:
-                return False
-    for i in range(0, len(solution)):
-        r.clear()
-        for j in range(0, len(q)):
-            r.append(solution[j][i])
-        for t in range(0, len(q)):
-            if r.count(q[i]) > 1:
-                return False
-    for i in range(0, len(solution)):
-        for j in range(0, len(solution[0])):
-            if solution[i][j] == ".":
-                return False
-    # block = get_block(grid,)
-
-    return True
+    pass
 
 
 if __name__ == "__main__":
