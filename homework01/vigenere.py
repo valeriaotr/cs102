c0048924cbb82bbb568a292911d72d26afc4cb7b
valeriaotr <valeryaotr@gmail.com>
2022-12-01 16:06:17 +0300

    vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,43 +1,26 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     ciphertext = ""
-    i = len(keyword)
-    for symbol in plaintext:
-        if len(keyword) < i + 1:
-            i = 0
-        if symbol.isupper():
-            symbol_index = ord(symbol) - ord("A")
-            new_symbol = chr((symbol_index + (ord(keyword[i]) - ord("A"))) % 26 + ord("A"))
-            ciphertext += new_symbol
-        elif symbol.islower():
-            symbol_index = ord(symbol) - ord("a")
-            new_symbol = chr((symbol_index + (ord(keyword[i]) - ord("a"))) % 26 + ord("a"))
-            ciphertext += new_symbol
-        elif symbol.isdigit():
-            ciphertext += str(symbol)
+    len_alphabet = 26
+    keyword_plus = [keyword[index % len(keyword)] for index in range(len(plaintext))]
+    for index, symbol in enumerate(plaintext):
+        up_low = ord("A") if symbol.isupper() else ord("a")
+        symbol = plaintext[index]
+        if symbol.isalpha():
+            ciphertext += chr((ord(symbol) + (ord(keyword_plus[index]) % up_low) - up_low) % len_alphabet + up_low)
         else:
             ciphertext += symbol
-
-        i += 1
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     plaintext = ""
-    i = len(keyword)
-    for symbol in ciphertext:
-        if len(keyword) < i + 1:
-            i = 0
-        if symbol.isupper():
-            symbol_index = ord(symbol) - ord("A")
-            new_symbol = chr((symbol_index - (ord(keyword[i]) - ord("A"))) % 26 + ord("A"))
-            plaintext += new_symbol
-        elif symbol.islower():
-            symbol_index = ord(symbol) - ord("a")
-            new_symbol = chr((symbol_index - (ord(keyword[i]) - ord("a"))) % 26 + ord("a"))
-            plaintext += new_symbol
-        elif symbol.isdigit():
-            plaintext += str(symbol)
+    len_alphabet = 26
+    keyword_plus = [keyword[i % len(keyword)] for i in range(len(ciphertext))]
+    for index, symbol in enumerate(ciphertext):
+        up_low = ord("A") if symbol.isupper() else ord("a")
+        symbol = ciphertext[index]
+        if symbol.isalpha():
+            plaintext += chr((ord(symbol) - (ord(keyword_plus[index]) % up_low) - up_low) % len_alphabet + up_low)
         else:
             plaintext += symbol
-        i += 1
     return plaintext
