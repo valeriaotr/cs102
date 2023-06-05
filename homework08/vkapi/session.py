3696cac2d8a91569ec8144e92b82d6697201734d
valeriaotr <valeryaotr@gmail.com>
2023-06-05 10:22:47 +0300

    vkapi3

--- homework08/vkapi/session.py
+++ homework08/vkapi/session.py
@@ -8,20 +8,20 @@ from requests.packages.urllib3.util.retry import Retry  # type: ignore
 
 class Session:
     """
-     Сессия.
+    Сессия.
 
-     :param base_url: Базовый адрес, на который будут выполняться запросы.
-     :param timeout: Максимальное время ожидания ответа от сервера.
-     :param max_retries: Максимальное число повторных запросов.
-     :param backoff_factor: Коэффициент экспоненциального нарастания задержки.
-     """
+    :param base_url: Базовый адрес, на который будут выполняться запросы.
+    :param timeout: Максимальное время ожидания ответа от сервера.
+    :param max_retries: Максимальное число повторных запросов.
+    :param backoff_factor: Коэффициент экспоненциального нарастания задержки.
+    """
 
     def __init__(
-            self,
-            base_url: str,
-            timeout: float = 5.0,
-            max_retries: int = 3,
-            backoff_factor: float = 0.3,
+        self,
+        base_url: str,
+        timeout: float = 5.0,
+        max_retries: int = 3,
+        backoff_factor: float = 0.3,
     ) -> None:
         self.base_url = base_url
         self.timeout = timeout
@@ -43,9 +43,7 @@ class Session:
 
         if "timeout" in kwargs:
             self.timeout = kwargs["timeout"]
-        response = self.session.get(
-            self.base_url + "/" + url, timeout=self.timeout, *args, **kwargs
-        )
+        response = self.session.get(self.base_url + "/" + url, timeout=self.timeout, *args, **kwargs)
 
         return response
 
@@ -53,7 +51,5 @@ class Session:
 
         if "timeout" in kwargs:
             self.timeout = kwargs["timeout"]
-        response = self.session.post(
-            self.base_url + "/" + url, timeout=self.timeout, *args, **kwargs
-        )
+        response = self.session.post(self.base_url + "/" + url, timeout=self.timeout, *args, **kwargs)
         return response
