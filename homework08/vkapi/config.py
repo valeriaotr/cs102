a7b010c99b298a74d2f720ac678db3295d3cc0e1
valeriaotr <valeryaotr@gmail.com>
2023-06-07 19:47:53 +0300

    final

---
+++ homework08/vkapi/config.py
@@ -0,0 +1,7 @@
+# https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
+
+VK_CONFIG = {
+    "domain": "https://api.vk.com/method",
+    "access_token": "vk1.a.mKcsue3wDu0fzM6awE3zYX1D6KYHX9BdblHvBZiElT1lozKEJOUfmcHriySTc1w7Fqnw_rjz2-8dyhZ05RnaauLn34p4hbnPkr7Ecg6Y-Lqd7XQMnlaAwv6lZhcZo3tJqAHv6XrGPY8LNDshKdZsUBxoCgsoKK0nTyJBlGvDfCcQD5d6iRoCNceharJHRobv",
+    "version": "5.126",
+}
